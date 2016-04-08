7c5e94c80bd74254cd5205b853b75d1591d07652
FourSeasonsHVAC <fshaclb@fourseasonslicensedbusiness.com>
2016-04-08 13:36:12 -0500

    Update dropdowns.js

--- dropdowns.js
+++ dropdowns.js
@@ -2,6 +2,6 @@
 $(document).ready(function(){
   $('#ac').click(function(item) {
     $('#ac1ModelNum').html(item.target.id)
-    console.log(item.child)
+    console.log($('#ac'.index(item))
   });
 });
